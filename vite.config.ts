70a7c8e8d612fe92a7766535317869d52f9a9679
raccoonlinlin <raccoonlinlin20010119@gmail.com>
2026-08-09 17:30:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/raccoonlinlin-store/',
-  plugins: [react()],
-});
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+})
